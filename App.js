1bf848c1b8e4ece901cdd0b0368d284c57521bc2
Elisa Morais <elisa.morais74@gmail.com>
2022-10-10 23:40:16 -0300

    Iniciando projeto

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { StatusBar } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App () {
+  return (
+    <View style={styles.container}>
+      <Text>React</Text>
+      
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+
+  },
+});
+
